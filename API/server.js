15ac2aed72bfb313b17029e9f06be4520d4f1758
Fellipe Reis <reisfellipe11@gmail.com>
2024-06-30 17:09:06 -0300

    saving progress

--- API/server.js
+++ API/server.js
@@ -9,18 +9,22 @@ OBJETIVO DESSE BACK-END: Criar nossa API de usuários
 import express from 'express'
 
 const app = express();
+app.use(express.json());//avisando pro express que usaremos arquivo .JSON
 
 const users = [];
 
 app.post('/users', (req,res)=>{
 
-    console.log(req)
-    res.send('Ok POST')
+    users.push(req.body)
+
+    res.send('Ok deu certo')
 
 })
 
 app.get('/users', (req, res) => {
     app.send('método GET está respondendo')
+
+    res.json(users)
 })
 
 app.listen(3000)
