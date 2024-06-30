196099c8398b65d5761d07e7784829671e1be3df
Fellipe Reis <reisfellipe11@gmail.com>
2024-06-30 16:36:11 -0300

    using method post

--- API/server.js
+++ API/server.js
@@ -1,6 +1,18 @@
+/*
+OBJETIVO DESSE BACK-END: Criar nossa API de usuários
+    - Criar um usuário
+    - Listar todos os usuários
+    - Editar um usuário
+    - Deletar um usuário
+*/
+
 import express from 'express'
 
 const app = express();
+
+const users = [];
+
+app.post('/users')
 
 app.get('/users', (requisition, response) => {
     app.send('método GET está respondendo')
