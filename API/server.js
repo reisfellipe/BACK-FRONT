3e5d2b491639642b6eb66a22f0f161fcce5cce8d
Fellipe Reis <reisfellipe11@gmail.com>
2024-07-02 23:14:17 -0300

    creating more methods http

--- API/server.js
+++ API/server.js
@@ -32,9 +32,49 @@ app.post('/users', async (req,res)=>{
 
 app.get('/users', async (req, res) => {
 
-    const users = await prisma.user.findMany()
+    let users = [];
+
+    if(req.query) {
+        users = await prisma.user.findMany({
+            where:{
+                name:req.query.name,
+                email:req.query.email,
+                age:req.query.age
+            }
+        })
+    }else(
+        users = await prisma.user.findMany()
+    )
+
 
     res.status(200).json(users)
+})
+
+app.put('/usuarios/:id',  async(req,res) => {
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    
+    res.status(201).json(req.body)
+})
+
+app.delete('usuarios/:id', async (req, res) =>{
+    
+    await prisma.user.delete({
+        where: {
+            id: req.body.id
+        }
+    })
+
+    res.status(200).json({message:"User deleted succesfully"})
 })
 
 app.listen(3000)
