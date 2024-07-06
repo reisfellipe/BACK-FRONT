c7112b81c85d56d66f53f4d42e36d5c52d3988ea
Fellipe Reis <reisfellipe11@gmail.com>
2024-07-06 19:11:37 -0300

    adjust

--- onlydevs-userSide/src/main.jsx
+++ onlydevs-userSide/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/index'
+import Home from './pages/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
