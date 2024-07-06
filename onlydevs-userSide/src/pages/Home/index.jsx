6ec21cf46dd7fa1f826876a24784f2682d208768
Fellipe Reis <reisfellipe11@gmail.com>
2024-07-06 20:30:28 -0300

    fixing up the import images

--- onlydevs-userSide/src/pages/Home/index.jsx
+++ onlydevs-userSide/src/pages/Home/index.jsx
@@ -1,10 +1,41 @@
 import './style.css'
+import Trash from '../../assets/trash.png'
+import Brand from '../../assets/onlyDevs.png'
 
 function Home() {
 
   return (
     <>
-      <h1>Olaa</h1>
+      <header>
+        <nav>
+          <div className="logo">
+            <h1>OnlyDevs+</h1>
+            <img src={Brand} />
+          </div>
+        </nav>
+      </header>
+
+      <div className="container">
+        <form>
+          <h1>Create your login</h1>
+          <input type="text" name="nome" placeholder='Your name'/>
+          <input type="number" name='idade' placeholder='Your age' aria-valuemin={18} />
+          <input type="email" name='email' placeholder='Your best e-mail' />
+          <button>Register me</button>
+        </form>
+
+        <div>
+          <div>
+            <p>Name:</p>
+            <p>Age:</p>
+            <p>Email:</p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>
+
+      </div>
     </>
   )
 }
