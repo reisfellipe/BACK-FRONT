a67530cd2efd7b7e682cdde0b8ca329295c5cfb6
Fellipe Reis <reisfellipe11@gmail.com>
2024-07-06 21:54:03 -0300

    creating stylishing

--- onlydevs-userSide/src/pages/Home/index.jsx
+++ onlydevs-userSide/src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import './style.css'
 import Trash from '../../assets/trash.png'
 import Brand from '../../assets/onlyDevs.png'
+import Flash from '../../assets/flash.png'
+import Star from '../../assets/star.png'
 
 function Home() {
 
@@ -9,33 +11,71 @@ function Home() {
       <header>
         <nav>
           <div className="logo">
-            <h1>OnlyDevs+</h1>
+            <h1>Only<span>Devs</span></h1>
             <img src={Brand} />
+          </div>
+          <div className="navigator">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Plans</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">
+                    <span>Login</span>
+                  </a>
+              </li>
+            </ul>
           </div>
         </nav>
       </header>
 
-      <div className="container">
-        <form>
-          <h1>Create your login</h1>
-          <input type="text" name="nome" placeholder='Your name'/>
-          <input type="number" name='idade' placeholder='Your age' aria-valuemin={18} />
-          <input type="email" name='email' placeholder='Your best e-mail' />
-          <button>Register me</button>
-        </form>
+      <section className="copy">
 
-        <div>
-          <div>
-            <p>Name:</p>
-            <p>Age:</p>
-            <p>Email:</p>
+        <div className="text">
+          <h2>Our platform exclusive for <span>Developers</span> is available now!</h2>
+          <p>Figure out another projects publisheds for teams of programming, create your own communities to share knowledge and get up a pair programming. You will know average ways to do structures using the technologies as <span>HTML, CSS, Javascript, React.JS, Node.JS, MongoDB Atlas, Prisma, SAAS, Bootstrap, Typescript</span> and so much others.
+          </p>
+
+          <div className="text-links">
+            <div className="link-st">
+              <div className="description">
+                <h3>Get DevClub+</h3>
+                <p>Where you can find templates created for experts like you</p>
+              </div>
+              <img src={Flash} />
+            </div>
+
+            <div className="link-nd">
+              <h3>Get Templates PRO</h3>
+              <p>Done for own influencers of pair programming published on Figma exclusive</p>
+              <img src={Star} />
+            </div>
           </div>
-          <button>
-            <img src={Trash} />
-          </button>
-        </div>
 
-      </div>
+        </div>
+        
+        <div className="container">
+          <form>
+            <h1>Create your login</h1>
+            <input type="text" name="nome" placeholder='Your name'/>
+            <input type="number" name='idade' placeholder='Your age' aria-valuemin={18} />
+            <input type="email" name='email' placeholder='Your best e-mail' />
+            <button>Register me</button>
+          </form>
+
+          <div>
+            <div>
+              <p>Name:</p>
+              <p>Age:</p>
+              <p>Email:</p>
+            </div>
+            <button>
+              <img src={Trash} />
+            </button>
+          </div>
+
+        </div>
+      </section>
+
     </>
   )
 }
